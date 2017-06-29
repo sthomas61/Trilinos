eb5e12dd4b7316c3281cb2644927418fd17603e4
Dan Ibanez <dan.a.ibanez@gmail.com>
2017-06-29 21:11:55 +0200

    Draft most of the new YamlParameterList reader

--- packages/teuchos/parameterlist/src/Teuchos_YamlParser.cpp
+++ packages/teuchos/parameterlist/src/Teuchos_YamlParser.cpp
@@ -57,6 +57,298 @@
 #include "Teuchos_YamlParameterListCoreHelpers.hpp"
 #include "Teuchos_TwoDArray.hpp"
 #include "Teuchos_YAML.hpp"
+#include "Teuchos_Array.hpp"
+#include "Teuchos_TwoDArray.hpp"
+
+namespace Teuchos {
+
+using PLPair = std::pair<std::string, ParameterEntry>;
+using Scalar = std::pair<bool, std::string>;
+
+bool operator==(PLPair const&, PLPair const&) { return false; }
+bool operator<<(std::ostream& os, PLPair const&) { return os; }
+
+bool operator==(Scalar const&, Scalar const&) { return false; }
+bool operator<<(std::ostream& os, Scalar const&) { return os; }
+
+namespace YamlParameterList {
+
+class Reader : public Teuchos::Reader {
+ public:
+  Reader():Teuchos::Reader(YAML::ask_reader_tables()) {
+  }
+  virtual ~Reader() {}
+ protected:
+  virtual void at_shift(any& result_any, int token, std::string& text) {
+    using std::swap;
+    switch (token) {
+      case YAML::TOK_RAW:
+      case YAML::TOK_SQUOTED:
+      case YAML::TOK_DQUOTED: {
+        Scalar& result = make_any_ref<Scalar>(result_any);
+        swap(result.second, text);
+        bool is_quoted = (token != YAML::TOK_RAW);
+        result.first = is_quoted;
+        break;
+      }
+    }
+  }
+  virtual void at_reduce(any& result_any, int prod, std::vector<any>& rhs) {
+    using std::swap;
+    switch (prod) {
+      case YAML::PROD_DOC: {
+        swap(result_any, rhs.at(2));
+        break;
+      }
+      case YAML::PROD_TOP_BMAP:
+      case YAML::PROD_TOP_BSEQ:
+      case YAML::PROD_BMAP: {
+        swap(result_any, rhs.at(1));
+        break;
+      }
+      case YAML::PROD_BSEQ: {
+        nested_array_to_2d_array(result_any, rhs.at(1));
+        break;
+      }
+      case YAML::PROD_TOP_BLOCK: {
+        swap(result_any, rhs.at(0));
+        break;
+      }
+      case YAML::PROD_BMAP_FIRST_ITEM:
+      case YAML::PROD_FMAP_FIRST_ITEM: {
+        map_first_item(result_any, rhs);
+        break;
+      }
+      case YAML::PROD_BMAP_NEXT_ITEM:
+      case YAML::PROD_FMAP_NEXT_ITEM: {
+        map_next_item(result_any, rhs);
+        break;
+      }
+      case YAML::PROD_BSEQ_FIRST_ITEM:
+      case YAML::PROD_FSEQ_FIRST_ITEM: {
+        seq_first_item(result_any, rhs);
+        break;
+      }
+      case YAML::PROD_BSEQ_NEXT_ITEM:
+      case YAML::PROD_FSEQ_NEXT_ITEM: {
+        seq_next_item(result_any, rhs);
+        break;
+      }
+      case YAML::PROD_BSEQ_SCALAR: {
+        swap(result_any, rhs.at(2));
+        break;
+      }
+      case YAML::PROD_BMAP_ITEM: {
+        map_item(result_any, rhs.at(1), rhs.at(5));
+        break;
+      }
+      case YAML::PROD_BMAP_SCALAR:
+      case YAML::PROD_BMAP_BLOCK:
+      case YAML::PROD_BMAP_FLOW: {
+        swap(result_any, rhs.at(0));
+        break;
+      }
+      case YAML::PROD_FSEQ_EMPTY: {
+        throw ParserFail("Empty arrays (sequences) not allowed!"
+            "\n(need to be able to deduce the member type)\n");
+      }
+      case YAML::PROD_FMAP_EMPTY: {
+        make_any_ref<ParameterList>(result_any);
+        break;
+      }
+      case YAML::PROD_FSEQ: {
+        nested_array_to_2d_array(result_any, rhs.at(2));
+        break;
+      }
+      case YAML::PROD_FMAP: {
+        swap(result_any, rhs.at(2));
+        break;
+      }
+      case YAML::PROD_FSEQ_SCALAR:
+      case YAML::PROD_FSEQ_FLOW:
+      case YAML::PROD_FMAP_SCALAR:
+      case YAML::PROD_FMAP_FLOW: {
+        swap(result_any, rhs.at(0));
+        break;
+      }
+      case YAML::PROD_FMAP_ITEM: {
+        map_item(result_any, rhs.at(0), rhs.at(4));
+        break;
+      }
+    }
+  }
+  void map_item(any& result_any, any& key_any, any& value_any) {
+    std::string& key = any_ref_cast<Scalar>(key_any).second;
+    PLPair& result = make_any_ref<PLPair>(result_any);
+    swap(result.first, key);
+    if (value_any.type() == typeid(Scalar)) {
+      Scalar& value_scalar = any_ref_cast<Scalar>(value_any);
+      bool value_quoted = value_scalar.first;
+      std::string& value_str = value_scalar.second;
+      if (!value_quoted && is_parseable_as<int>(value_str)) {
+        int value = parse_as<int>(value_str);
+        result.second = ParameterEntry<int>(value);
+      } else if (!value_quoted && is_parseable_as<double>(value_str)) {
+        double value = parse_as<double>(value_str);
+        result.second = ParameterEntry<double>(value);
+      } else {
+        result.second = ParameterEntry<std::string>(value_str);
+      }
+    } else if (value_any.type() == typeid(Array<int>)) {
+      Array<int>& value = any_ref_cast<Array<int> >(value_any);
+      result.second = ParameterEntry<Array<int> >(value);
+    } else if (value_any.type() == typeid(Array<double>)) {
+      Array<double>& value = any_ref_cast<Array<double> >(value_any);
+      result.second = ParameterEntry<Array<double> >(value);
+    } else if (value_any.type() == typeid(Array<std::string>)) {
+      Array<std::string>& value = any_ref_cast<Array<std::string> >(value_any);
+      result.second = ParameterEntry<Array<std::string> >(value);
+    } else if (value_any.type() == typeid(ParameterList)) {
+      ParameterList& value = any_ref_cast<ParameterList>(value_any);
+      ParameterList& result_pl = result.second.setList();
+      swap(result_pl, value);
+    } else {
+      throw ParserFail("unexpected YAML map value type");
+    }
+  }
+  template <typename T>
+  void nested_array_to_2d_array_tmpl(any& out, any& in) {
+    Array<Array<T> >& inval = any_ref_cast<Array<Array<T> > >(in);
+    TwoDArray<T>& outval = make_any_ref<TwoDArray<T> >(out);
+    for (Teuchos_Ordinal i = 0; i < inval.size(); ++i) {
+      if (inval[i].size() != inval[0].size()) {
+        throw ParserFail("2D array: sub-arrays are different sizes");
+      }
+    }
+    outval = TwoDArray<T>(inval.size(), inval[0].size());
+    for (Teuchos_Ordinal i = 0; i < outval.numRows(); ++i) {
+      for (Teuchos_Ordinal j = 0; j < outval.numCols(); ++j) {
+        swap(outval(i, j), inval[i][j]);
+      }
+    }
+  }
+  void nested_array_to_2d_array(any& result_any, any& rhs_any) {
+    using std::swap;
+    if (rhs_any.type() == typeid(Array<Array<int> >)) {
+      nested_array_to_2d_array_tmpl<int>(result_any, rhs_any);
+    } else if (rhs_any.type() == typeid(Array<Array<double> >)) {
+      nested_array_to_2d_array_tmpl<double>(result_any, rhs_any);
+    } else if (rhs_any.type() == typeid(Array<Array<std::string> >)) {
+      nested_array_to_2d_array_tmpl<std::string>(result_any, rhs_any);
+    } else {
+      swap(result_any, rhs_any);
+    }
+  }
+  void map_first_item(any& result_any, std::vector<any>& rhs) {
+    ParameterList& list = make_any_ref<ParameterList>(result_any);
+    PLPair& pair = any_ref_cast<PLPair>(rhs.at(0));
+    list.set(pair.first, pair.second);
+  }
+  void map_next_item(any& result_any, std::vector<any>& rhs) {
+    using std::swap;
+    swap(result_any, rhs.at(0));
+    ParameterList& list = any_ref_cast<ParameterList>(result_any);
+    PLPair& pair = any_ref_cast<PLPair>(rhs.at(2));
+    list.set(pair.first, pair.second);
+  }
+  void seq_first_item(any& result_any, std::vector<any>& rhs) {
+    using std::swap;
+    if (rhs.at(0).type() == typeid(Scalar)) {
+      Scalar& scalar = any_ref_cast<Scalar>(rhs.at(0));
+      bool scalar_quoted = scalar.first;
+      std::string& scalar_str = scalar.second;
+      if (!scalar_quoted && is_parseable_as<int>(scalar_str)) {
+        Array<int>& a = make_any_ref<Array<int> >(result_any);
+        int v = any_cast<int>(rhs.at(0));
+        a.push_back(v);
+      } else if (!scalar_quoted && is_parseable_as<double>(scalar_str)) {
+        Array<double>& a = make_any_ref<Array<double> >(result_any);
+        double v = any_cast<double>(rhs.at(0));
+        a.push_back(v);
+      } else {
+        Array<std::string>& a = make_any_ref<Array<std::string> >(result_any);
+        std::string& v = any_ref_cast<std::string>(rhs.at(0));
+        a.push_back(std::string());
+        swap(a.back(), v);
+      }
+    } else if (rhs.at(0).type() == typeid(Array<int>)) {
+      Array<Array<int> >& a = make_any_ref<Array<Array<int> > >(result_any);
+      Array<int>& v = any_ref_cast<Any<int> >(rhs.at(0));
+      a.push_back(Array<int>());
+      swap(a.back(), v);
+    } else if (rhs.at(0).type() == typeid(Array<double>)) {
+      Array<Array<double> >& a = make_any_ref<Array<Array<double> > >(result_any);
+      Array<double>& v = any_ref_cast<Any<double> >(rhs.at(0));
+      a.push_back(Array<double>());
+      swap(a.back(), v);
+    } else if (rhs.at(0).type() == typeid(Array<std::string>)) {
+      Array<Array<std::string> >& a = make_any_ref<Array<Array<std::string> > >(result_any);
+      Array<std::string>& v = any_ref_cast<Any<std::string> >(rhs.at(0));
+      a.push_back(Array<std::string>());
+      swap(a.back(), v);
+    } else {
+      throw Teuchos::ParserFail(
+          "unexpected sequence item type");
+    }
+  }
+  void seq_next_item(any& result_any, std::vector<any>& rhs) {
+    using std::swap;
+    swap(result_any, rhs.at(0));
+    if (result_any.type() == typeid(Array<int>)) {
+      Array<int>& a = any_ref_cast<Array<int> >(result_any);
+      Scalar& val = any_ref_cast<Scalar>(rhs.at(2));
+      if (val.first) throw ParserFail("string in integer array");
+      int v = parse_as<int>(val.second);
+      a.push_back(v);
+    } else if (result_any.type() == typeid(Array<double>)) {
+      Array<double>& a = any_ref_cast<Array<double> >(result_any);
+      Scalar& val = any_ref_cast<Scalar>(rhs.at(2));
+      if (val.first) throw ParserFail("string in integer array");
+      double v = parse_as<double>(val.second);
+      a.push_back(v);
+    } else if (result_any.type() == typeid(Array<std::string>)) {
+      Array<std::string>& a = any_ref_cast<Array<std::string> >(result_any);
+      std::string& v = any_ref_cast<Scalar>(rhs.at(2)).second;
+      a.push_back(std::string());
+      swap(a.back(), v);
+    } else if (result_any.type() == typeid(Array<Array<int> >)) {
+      Array<Array<int> >& a = any_ref_cast<Array<Array<int> > >(result_any);
+      Array<int>& v = any_ref_cast<Any<int> >(rhs.at(2));
+      a.push_back(Array<int>());
+      swap(a.back(), v);
+    } else if (result_any.type() == typeid(Array<Array<double> >)) {
+      Array<Array<double> >& a = any_ref_cast<Array<Array<double> > >(result_any);
+      Array<double>& v = any_ref_cast<Any<double> >(rhs.at(2));
+      a.push_back(Array<double>());
+      swap(a.back(), v);
+    } else if (result_any.type() == typeid(Array<Array<std::string> >)) {
+      Array<Array<std::string> >& a =
+        any_ref_cast<Array<Array<std::string> > >(result_any);
+      Array<std::string>& v = any_ref_cast<Any<std::string> >(rhs.at(2));
+      a.push_back(Array<std::string>());
+      swap(a.back(), v);
+    } else {
+      throw Teuchos::ParserFail(
+          "bug in YamlParameterList::Reader: unexpected sequence (array) type");
+    }
+  }
+ private:
+  template <typename T>
+  bool is_parseable_as(std::string const& text) {
+    std::istringstream ss(text);
+    T value;
+    return (ss >> value);
+  }
+  template <typename T>
+  T parse_as(std::string const& text) {
+    std::istringstream ss(text);
+    T value;
+    ss >> value;
+    return value;
+  }
+};
+
+}} // namespace Teuchos::YamlParameterList
 
 namespace Teuchos
 {
